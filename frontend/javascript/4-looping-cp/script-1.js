1965f60ea5ae23bfcdb3540344b49463011aa3e8
Muhammad Rifqi <muhammadrifqi142@gmail.com>
2022-03-23 17:01:03 +0700

    Answer: Answer Workshop JS Part 2 Looping

--- frontend/javascript/4-looping-cp/script-1.js
+++ frontend/javascript/4-looping-cp/script-1.js
@@ -12,6 +12,17 @@
 
 
 // Masukan jumlah bilangan yang ingin dicek, iterasi dari angka 1
-const counter = parseInt(prompt("Masukan jumlah bilangan yang ingin dicek: "));
+const counter = 15;
 
 // TODO: answer here
+    for (let i = 1; i <= counter; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(i + " adalah kelipatan 3 & 5");
+        } else if (i % 3 === 0) {
+            console.log(i + " adalah kelipatan 3");
+        } else if (i % 5 === 0) {
+            console.log(i + " adalah kelipatan 5");
+        } else {
+            console.log(i);
+    }
+}
--- frontend/javascript/4-looping-cp/script-2.js
+++ frontend/javascript/4-looping-cp/script-2.js
@@ -10,6 +10,17 @@
 // *
 
 // Masukan jumlah baris
-const n = parseInt(prompt("Masukan jumlah baris: "));
+const n = 5;
 
 // TODO: answer here
+function segitiga(n) {
+    let stars = '';
+    for (let i = 0; i < n; i++) {
+        for (let j = n; j > i; j--) {
+            stars += "*";
+        }
+        stars += "\n";
+    }
+    return stars;
+}
+console.log(segitiga(n));
