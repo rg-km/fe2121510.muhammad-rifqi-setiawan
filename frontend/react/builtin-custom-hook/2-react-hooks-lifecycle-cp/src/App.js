131e937de268b068411124ccb43ef43a913e955e
Muhammad Rifqi <muhammadrifqi142@gmail.com>
2022-05-10 11:44:41 +0700

    answer: react-lifecycle-cp

--- frontend/react/builtin-custom-hook/2-react-hooks-lifecycle-cp/src/App.js
+++ frontend/react/builtin-custom-hook/2-react-hooks-lifecycle-cp/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-
+ 
 export default function App() {
   return <RandomQuote />;
 }
-
+ 
 export function RandomQuote() {
-  // TODO: answer here
+  //beginanswer
+  const [quote, setQuote] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  //endanswer
   React.useEffect(() => {
-    // TODO: answer here
+    //beginanswer
+    getQuote();
+    //endanswer
   }, []);
-
+ 
   function getQuote() {
     setLoading(true);
     fetch("https://api.quotable.io/random")
@@ -17,6 +22,44 @@ export function RandomQuote() {
       .then((data) => setQuote(data))
       .finally(() => setLoading(false));
   }
-
-  // TODO: answer here
+ 
+  //beginanswer
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+        <a className="navbar-brand" href="/" data-id="brand">
+          Random Quote
+        </a>
+      </nav>
+      <div className="container p-3">
+        <div
+          className="d-flex flex-column justify-content-center align-items-center bg-warning"
+          style={{
+            borderTopLeftRadius: 100,
+            borderTopRightRadius: 100,
+            borderBottomRightRadius: 100,
+            padding: "5rem",
+          }}
+        >
+          <div className="d-flex justify-content-center">
+            {loading && "Loading..."}
+          </div>
+          <figure>
+            <blockquote className="blockquote">
+              <p className="display-4" data-testid="quote">
+                {quote && quote.content}.
+              </p>
+            </blockquote>
+            <figcaption className="blockquote-footer">
+              {quote && quote.author}
+            </figcaption>
+          </figure>
+          <button className="btn btn-primary" onClick={getQuote}>
+            Get another quote
+          </button>
+        </div>
+      </div>
+    </>
+  );
+  //endanswer
 }
