a5dbbabb49a3d04aee276b3e492c9c0846a1c62c
Muhammad Rifqi <muhammadrifqi142@gmail.com>
2022-05-17 09:18:02 +0700

    answer: react-app

--- frontend/react/react-project/src/components/LikeDislikeButton.js
+++ frontend/react/react-project/src/components/LikeDislikeButton.js
@@ -1,7 +1,12 @@
 // TODO: answer here
+import React from "react"
+import { useEffect, useState } from "react"
+import { getSession, auth } from "../api/auth"
+import { useContext } from "react"
+import { SessionContext } from "../context/SessionContext"
+
 export default function LikeDislikeButton({id, isLiked, isDisliked, likeCount, dislikeCount}) {
 
     // TODO: answer here
-    
     
 }
