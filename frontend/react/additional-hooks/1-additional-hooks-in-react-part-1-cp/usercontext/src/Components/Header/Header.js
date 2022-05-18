a722d78c8f7aee7c3b98d08ef77367589abd4fd5
Muhammad Rifqi <muhammadrifqi142@gmail.com>
2022-05-18 09:04:27 +0700

    answer: usercontext-cp

--- frontend/react/additional-hooks/1-additional-hooks-in-react-part-1-cp/usercontext/src/Components/Header/Header.js
+++ frontend/react/additional-hooks/1-additional-hooks-in-react-part-1-cp/usercontext/src/Components/Header/Header.js
@@ -1,20 +1,28 @@
 import styles from './Header.module.scss';
 import { ThemeContext } from '../../App';
 import { useContext } from 'react';
-
+ 
 const Header = ({text}) =>{
     const theme = useContext(ThemeContext).theme;
-
+ 
     const renderTheme = (theme) =>{
         //mengubah warna dari Header berdasarkan theme
         // TODO: answer here
+        if (theme === "dark") {
+            return {
+                styles: styles['dark-theme']
+            }
+        }
+        return {
+            styles: styles['light-theme']
+        }
     }
-
+ 
     return(
-        <h1 data-testid="header" className={`${styles['header']} ${renderTheme(theme)}`}>
+        <h1 data-testid="header" className={`${styles['header']} ${renderTheme(theme).styles}`}>
             {text}
         </h1>
     )
 }
-
+ 
 export default Header;
