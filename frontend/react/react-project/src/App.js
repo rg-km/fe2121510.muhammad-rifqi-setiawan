3d52a47d03ad802544b5078d82093cdb15ff0816
Muhammad Rifqi <muhammadrifqi142@gmail.com>
2022-05-22 23:18:44 +0700

    answer: answer React-Project

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -1,18 +1,17 @@
 import "./App.css"
 // TODO: answer here
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "./api/config";
 import Navbar from "./components/Navbar";
 import PostCard from "./components/PostCard";
 import UploadForm from "./components/UploadForm";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { API_URL } from "./api/config";
 
 function App() {
   // TODO: answer here
   const [listPost, setListPost] = useState([])
-
-  const fetchListPost = async () => {
+const fetchListPost = async () => {
     try {
       const res = await axios.get(`${API_URL}/post/list`, {
         withCredentials: true,
@@ -26,11 +25,30 @@ function App() {
   useEffect(() => {
     fetchListPost()
   }, [])
+
   return (
     <div aria-label="App" className="App">
       <Navbar />
-      <PostCard />
-      <UploadForm />
+      <div className="card-wrapper">
+        {listPost.map((post) => (
+          <PostCard
+          image={post.image}
+          username={post.author.name} 
+          caption={post.content} 
+          date={post.createdAt} 
+          isLiked={post.liked} 
+          isDisliked={post.disliked} 
+          likeCount={post.likeCount} 
+          dislikeCount={post.dislikeCount} 
+          id={post.id}
+          refetchListPost={fetchListPost}
+          />
+        ))}
+        </div>
+      <UploadForm/>
+      {/* <header className="App-header">
+        <h1 aria-label="App Title">Instagram Clone</h1>
+      </header> */}
     </div>
   );
 }
--- frontend/react/react-project/src/components/LikeDislikeButton.js
+++ frontend/react/react-project/src/components/LikeDislikeButton.js
@@ -1,70 +1,100 @@
 // TODO: answer here
 import React from "react"
+import axios from "axios";
+import { API_URL } from "../api/config"
 import { useEffect, useState } from "react"
 import { getSession, auth } from "../api/auth"
-import { API_URL } from "../api/config"
 import { useContext } from "react"
 import { SessionContext } from "../context/SessionContext"
 
 export default function LikeDislikeButton({
     id, 
     image,
-    isLiked, 
+    isLiked = 0 , 
     isDisliked = 0, 
     likeCount = 0, 
-    dislikeCount}) {
+    dislikeCount = 0,
+    refetchListPost,
+}) {
     
-    
+ 
     // TODO: answer here
     const [like, setLike] = useState(likeCount);
     const [dislike, setDislike] = useState(dislikeCount);
-    const [isLikedButton, setIsLiked] = useState(isLiked);
-    const [isDislikedButton, setIsDisliked] = useState(isDisliked);
+    const [isLikedButton, setIsLikedButton] = useState(isLiked);
+    const [isDislikedButton, setIsDislikedButton] = useState(isDisliked);    
     
-    const handleLikeButton = () => {
-        if (isLikedButton && !isDislikedButton){
-            setIsLiked(false);
-            setLike(like - 1);
-        } else if (!isLikedButton && !isDislikedButton){
-            setIsLiked(true);
-            setLike(like + 1);
-        } else {
-            setIsLiked(true);
-            setIsDisliked(false);
-            setLike(like + 1);
-            setDislike(dislike - 1);
+    const handleLikeButton = async () => {
+        try {
+            let res
+            if(!isLikedButton){
+                res = await axios.get(`${API_URL}/post/${id}/like`,{
+                    withCredentials: true,
+                })
+                if(!isDislikedButton){
+                    setLike(like + 1);
+                    setIsLikedButton(true);
+                }else{
+                    setLike(like + 1);
+                    setIsLikedButton(true);
+                    setDislike(dislike - 1);
+                    setIsDislikedButton(false);
+                }
+            }else{
+                res = await axios.get(`${API_URL}/post/${id}/unlike`,{
+                    withCredentials: true,
+                })
+                setLike(like - 1);
+                setIsLikedButton(false);
+            }
+            // refetchListPost()
+        } catch (error) {
+            console.log(error)
+        }finally{
+            // refetchListPost()
         }
     }
-
-    const handleDislikeButton = () => {
-        if (!isLikedButton && isDislikedButton){
-            setIsDisliked(false);
-            setDislike(dislike - 1);
-        } else if (!isLikedButton && !isDislikedButton){
-            setIsDisliked(true);
-            setDislike(dislike + 1);
-        } else {
-            setIsDisliked(true);
-            setIsLiked(false);
-            setDislike(dislike + 1);
-            setLike(like - 1);
+ 
+    const handleDislikeButton = async () => {
+        try {
+            let res
+            if(!isDislikedButton){
+                res = await axios.get(`${API_URL}/post/${id}/dislike`,{
+                    withCredentials: true,
+                })
+                if(!isLikedButton){
+                    setDislike(dislike + 1);
+                    setIsDislikedButton(true);
+                }else{
+                    setDislike(dislike + 1);
+                    setIsDislikedButton(true);
+                    setLike(like - 1);
+                    setIsLikedButton(false);
+                }
+            }else{
+                res = await axios.get(`${API_URL}/post/${id}/undislike`,{
+                    withCredentials: true,
+                })
+                setDislike(dislike - 1);
+                setIsDislikedButton(false);
+            }
+            // refetchListPost()
+        } catch (error) {
+            console.log(error)
+        }finally{
+            // refetchListPost()
         }
     }
     
     return (
-        <div aria-label="Like & Dislike Button" className="LikeDislikeButton">
-            <button aria-label="Like Button" onClick={() => {
-                handleLikeButton();                
-            }}
-            >Like</button>
-            <button aria-label="Dislike Button" onClick={() => {
-                handleDislikeButton();
-                
-            }}
-            >Dislike</button>
-            <p aria-label="Like Count">{like}</p>
-            <p aria-label="Dislike Count">{dislike}</p>
+        <div aria-label="Button">
+            <div className="ForButton">
+            <button aria-label="Like Button" onClick={handleLikeButton}
+            >Like <p aria-label="Like Count">{like}</p></button>
+
+            <button aria-label="Dislike Button" onClick={handleDislikeButton}
+            >Dislike <p aria-label="Dislike Count">{dislike}</p></button>
             </div>
-            
+        </div>
     );
 }
